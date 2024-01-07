65ee3e79223c0983d6e2a434a69563aa3d0f8708
BuildTools <unconfigured@null.spigotmc.org>
2024-01-08 02:37:10 +0500

    release

---
+++ src/main/java/org/karmash/anicloudlbwfixes/AnicloudLBWFixes.java
@@ -0,0 +1,17 @@
+package org.karmash.anicloudlbwfixes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AnicloudLBWFixes extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
